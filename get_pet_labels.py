50319da43f27ec8f920641ac058694e99f3e4776
BeastMode06 <100443686+BeastMode06@users.noreply.github.com>
2022-03-05 12:16:34 -0500

    Update get_pet_labels.py

--- get_pet_labels.py
+++ get_pet_labels.py
@@ -46,17 +46,8 @@ def get_pet_labels(image_dir):
     # Creates empty dictionary for the results (pet labels, etc.)
     results_dic = dict()
     
-    # Print 10 of the filenames from folder specified as image_dir/
-    #print("\nPrints 10 filenames from folder specified as image_dir")
-    #for idx in range (0, min(10, len(in_files)), 1):
-            #print("{:2d} file: {:>25}".format(idx + 1, in_files[idx]))
-    
-    
-    
-    # Determine number of items in dictionary
-   # items_in_dic = len(results_dic)
-    #print("\nEmpty Dictionary results_dic - n items=", items_in_dic)
-          
+         
+                
     # Add new key-value pairs to dictionary ONLY when key doesn't already exist. 
     # This dictionary's value is a list that contains only 1 item - the pet image label
     
